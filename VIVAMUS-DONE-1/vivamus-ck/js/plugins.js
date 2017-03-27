f92a9299c402360fa8ea7931cafcb119b8241419
aljunma <contact@aljunmajo.com>
2017-03-27 18:13:40 +0800

    done html and css part only

---
+++ VIVAMUS-DONE-1/vivamus-ck/js/plugins.js
@@ -0,0 +1,33 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+	/* Detect Mac/PC - Being used as subpixel-antialiased rendering
+on Mac Webkit.
+-------------------------------------------------------------- */
+if (navigator.userAgent.indexOf('Mac OS X') !== -1) {
+  document.body.className += " mac";
+} else {
+  document.body.className += " pc";
+}
+
+// ************* Place any jQuery/helper plugins in here ******************** //
